458e526722e03d01ce21cb9b97e15973b8dfdf59
松榮美帆 <>
2020-06-30 15:14:01 +0900

    [Start]3-3~[Finish]3-11

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '12af07f037872c2ca684c7e194a2da2c7de25ecd1f83183b62845f90266f70b84dc34b22da31aff796c4eb1e38c6b8c66e37e0a44b614c019da15e2017d3516f'
